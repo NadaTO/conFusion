8325e814f22fd4eda3a3d63a226e586299155e26
Nada TOUIL <nada.touil@telecom-sudparis.eu>
2020-04-21 18:30:25 +0200

    angular router basic

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,16 @@
+//just for the sake of code clearness
+// that could be written directly in app-routing.module.ts
+import { Routes } from '@angular/router';
+
+import { MenuComponent } from '../menu/menu.component';
+import { DishdetailComponent } from '../dishdetail/dishdetail.component';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component';
+
+
+export const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'menu', component: MenuComponent},
+  {path:'', redirectTo: '/home', pathMatch:'full'}
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,27 +7,37 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
-import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+
+import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component'
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
     MatToolbarModule,
     FlexLayoutModule,
     MatListModule,
