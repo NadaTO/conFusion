e8e27f1ba1efd5ab252e87b8b776587b2892f743
Nada TOUIL <nada.touil@telecom-sudparis.eu>
2020-04-22 12:14:00 +0200

    add a leader service : assignment 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -11,6 +11,7 @@ import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes = [
   {path:'home', component: HomeComponent},
+  {path:'about', component: AboutComponent},
   {path:'menu', component: MenuComponent},
   {path:'contactus', component: ContactComponent},
   { path: 'dishdetail/:id',     component: DishdetailComponent },
